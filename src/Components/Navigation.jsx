7741ddc7879924c150082e9e66e179416dbc632c
Bibek Kr. Sah <45379856+bbk1918@users.noreply.github.com>
2021-03-03 14:37:19 +0530

    Update

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,28 +1,30 @@
 import React, { Component } from "react";
 import { Navbar, Nav, Button, Form, FormControl } from "react-bootstrap";
 import { FaCartPlus } from 'react-icons/fa';
-import { MdSearch } from "react-icons/md";
+import { MdSearch, MdAccountCircle } from "react-icons/md";
+import { BiLogIn } from "react-icons/bi";
 
 class TopNav extends Component {
   state = {};
   render() {
     return (
-      <div backgroundColor= "black">
-      <Navbar bg="primary" variant="dark">
+      <div >
+      <Navbar style={{backgroundColor:"#004d40"}}
+       variant="dark">
         <Navbar.Brand href="#home">
           <img src="/eMart.png" height="30" alt="Logo"/>
         </Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#features">About</Nav.Link>
-          <Nav.Link href="#pricing">Contact</Nav.Link>
-          <Nav.Link href="#pricing">Sign In</Nav.Link>
-          <Nav.Link href="#pricing">Sign Up</Nav.Link>
+          <Nav.Link href="#pricing">Contact</Nav.Link>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <Nav.Link href="#pricing"><BiLogIn size="25"/>&nbsp;Sign In</Nav.Link>
+          <Nav.Link href="#pricing"><MdAccountCircle size="25"/>&nbsp;Sign Up</Nav.Link>
         </Nav>
 
         <Form inline>
           <FormControl type="text" placeholder="Search Product..." className="mr-sm-2" />
-          <Button variant="outline-light"> <MdSearch/> </Button> &nbsp;
+          <Button variant="outline-light"> <MdSearch/> </Button> &nbsp;&nbsp;
         </Form>
           <button type="button" class="btn btn-primary" style = {{align:"right"}}>
             Cart &nbsp;<FaCartPlus/> <span class="badge badge-light">9</span>
